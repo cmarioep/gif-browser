72da9517ba0b3a66d1dab8adc853b483ff5753fa
cmarioep <cmescandonpardo@gmail.com>
2022-07-10 17:35:22 -0500

    New project: Gif Browser

---
+++ src/GifBrowser.jsx
@@ -0,0 +1,8 @@
+
+export default function componentName() {
+  return (
+    <>
+        <h1>Gif Browser</h1>
+    </>
+  );
+}
---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import GifBrowser from './GifBrowser';
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <GifBrowser />
+  </React.StrictMode>
+)
