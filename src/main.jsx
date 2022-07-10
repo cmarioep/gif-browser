074f2de1b846cf6a107f0e34621171d93d31cbe4
cmarioep <cmescandonpardo@gmail.com>
2022-07-10 17:47:22 -0500

    Feat: Basic global styles

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import GifBrowser from './GifBrowser';
 
+import './styles.css';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
